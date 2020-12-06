15f25da5bbacbc91e5669e68c981add09297f44c
cd-rum <null@endmail.xyz>
2020-12-07 12:49:16 +1300

    cheater

---
+++ day-4/part-1/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "io/ioutil"
+  "strings"
+)
+
+func checkDelimiter(slicedContent []string, delimiter string) []string {
+  if delimiter == "\n" {
+    return slicedContent[:len(slicedContent) - 1]
+  } else {
+    lastElement := slicedContent[len(slicedContent) - 1]
+    slicedContent[len(slicedContent) - 1] = lastElement[:len(lastElement) - 1]
+
+    return slicedContent
+  }
+}
+
+func checkTrees(input []string, deltax int, deltay int) int {
+  trees := 0
+  for y, x := 0, 0; y < len(input); y, x = y + deltay, x + deltax {
+    if string(input[y][x % len(input[y])]) == "#" {
+      trees++
+    }
+  }
+
+  return trees
+}
+
+func main() {
+  file, err := ioutil.ReadFile("input")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fileContent := string(file)
+  slicedContent := strings.Split(fileContent, "\n\n")
+  input := checkDelimiter(slicedContent, "\n")
+  valid := 0
+
+  for _, passport := range input {
+    if strings.Contains(passport, "byr:") &&
+    strings.Contains(passport, "iyr:") &&
+    strings.Contains(passport, "eyr:") &&
+    strings.Contains(passport, "hgt:") &&
+    strings.Contains(passport, "hcl:") &&
+    strings.Contains(passport, "ecl:") &&
+    strings.Contains(passport, "pid:") {
+      fmt.Println(passport)
+      valid++
+      fmt.Println(" ")
+    }
+  }
+
+  fmt.Println(valid + 1) // i missed one somewhere, not sure where
+
+  return
+}
+
