3343384b55a212ebd9c488564a3d94f921f7288d
cd-rum <null@endmail.xyz>
2020-12-07 12:04:34 +1300

    change input to read whole file

--- day-3/part-1/main.go
+++ day-3/part-1/main.go
@@ -1,50 +1,44 @@
 package main
 
 import (
-  "os"
   "fmt"
   "log"
-  "regexp"
-  "bufio"
-  "strconv"
+  "io/ioutil"
   "strings"
 )
 
-type password struct {
-  min, max         int
-  letter, password string
+func checkDelimiter(slicedContent []string, delimiter string) []string {
+  if delimiter == "\n" {
+		return slicedContent[:len(slicedContent) - 1]
+	} else {
+		lastElement := slicedContent[len(slicedContent) - 1]
+		slicedContent[len(slicedContent) - 1] = lastElement[:len(lastElement) - 1]
+
+		return slicedContent
+	}
 }
 
 func main() {
-  file, err := os.Open("input")
+  file, err := ioutil.ReadFile("input")
   if err != nil {
-      log.Fatal(err)
+    log.Fatal(err)
   }
 
-  passwords := []*password{}
-  inputRegex := regexp.MustCompile(`^(\d+)-(\d+) (\w): (\w+)$`)
+	fileContent := string(file)
+	slicedContent := strings.Split(fileContent, "\n")
+  input := checkDelimiter(slicedContent, "\n")
 
-  scanner := bufio.NewScanner(file)
-  for scanner.Scan() {
-    text := scanner.Text()
-    min, _ := strconv.Atoi(inputRegex.ReplaceAllString(text, "$1"))
-    max, _ := strconv.Atoi(inputRegex.ReplaceAllString(text, "$2"))
-    passwords = append(passwords, &password{
-      min:      min,
-      max:      max,
-      letter:   inputRegex.ReplaceAllString(text, "$3"),
-      password: inputRegex.ReplaceAllString(text, "$4"),
-    })
-  }
+  deltax := 3
+  deltay := 1
+  trees := 0
 
-  correct := 0
-  for _, pw := range passwords {
-    if count := strings.Count(pw.password, pw.letter); count >= pw.min && count <= pw.max {
-      correct++
+  for y, x := 0, 0; y < len(input); y, x = y + deltay, x + deltax {
+    if string(input[y][x % len(input[y])]) == "#" {
+      trees++
     }
   }
 
-  fmt.Println(correct)
+  fmt.Println(trees)
   return
 }
 
